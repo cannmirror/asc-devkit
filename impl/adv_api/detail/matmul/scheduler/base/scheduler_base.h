90351c662d55b1d7756cc9a2a5c78ba1a0efbd15
hyolee <lihongbo14@huawei.com>
2025-11-28 09:31:15 +0800

    fix F/S16S8 precision error
    Co-authored-by: hyolee<lihongbo14@huawei.com>

--- impl/adv_api/detail/matmul/scheduler/base/scheduler_base.h
+++ impl/adv_api/detail/matmul/scheduler/base/scheduler_base.h
@@ -116,9 +116,8 @@ public:
         }
         MATMUL_MODULE(MatmulQuantProcessor)->Init(tiling.GetBaseN());
 #if __NPU_ARCH__ == 5102
-        if constexpr (IsSameTypeV<SrcAT, half> && (IsSameTypeV<DstT, half> || IsSameTypeV<DstT, bfloat16_t>)) {
-            constexpr float FIX_VAL_RECIPROCAL = 1.0f / (1 << 16);
-            const uint64_t quantScalar = static_cast<const uint64_t>(*reinterpret_cast<const int32_t *>(&FIX_VAL_RECIPROCAL));
+        if constexpr (IsSameTypeV<SrcAT, half> && IsSameTypeV<SrcBT, half> && IsTypeOneOfV<DstT, half, bfloat16_t>) {
+            constexpr uint64_t quantScalar = 1065353216;
             MATMUL_MODULE(MatmulQuantProcessor)->SetQuantScalar(quantScalar);
         }
 
--- tests/unit/basic_api/third_party/json.cmake
+++ tests/unit/basic_api/third_party/json.cmake
@@ -13,6 +13,7 @@ if (NOT EXISTS "${JSON_PATH}/include/nlohmann/json.hpp")
         URL_HASH SHA256=0d8ef5af7f9794e3263480193c491549b2ba6cc74bb018906202ada498a79406
         DOWNLOAD_DIR ${third_party_TEM_DIR}
         SOURCE_DIR "${JSON_PATH}"  # 直接解压到此目录
+        TLS_VERIFY OFF
     )
     FetchContent_MakeAvailable(${JSON_NAME})
 endif()
