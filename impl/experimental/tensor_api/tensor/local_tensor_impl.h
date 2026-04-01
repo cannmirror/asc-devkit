1b2166b8f9fa05334a1e005cc32fe5e415b25060
yue_mo_du_qian <gaoming39@huawei.com>
2026-04-01 15:41:05 +0800

    tensor_api_experimental_l2cache
    Co-authored-by: gaoming<gaoming39@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1371 merge tensor_api_experimental_l2cache into tensor_api_experimental
    
    tensor_api_experimental_l2cache
    
    Created-by: yue_mo_du_qian
    Commit-by: gaoming
    Merged-by: cann-robot
    Description: ## 描述
    本次PR为张量计算引擎新增了L2缓存控制功能。主要目的是允许用户根据不同的计算模式（如禁用、普通）动态设置存储迭代器的L2缓存行为，从而在特定硬件架构下优化内存访问性能。
    
    ## 关联的Issue
    无
    
    ## 测试
    UT验证
    
    ## 文档更新
    无
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 fix: Bug 修复
    - [x] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!1371

--- impl/experimental/tensor_api/tensor/local_tensor_impl.h
+++ impl/experimental/tensor_api/tensor/local_tensor_impl.h
@@ -156,6 +156,10 @@ struct LocalTensor<TensorAttribute<EngineType, LayoutType>> {
     __aicore__ inline constexpr auto Tile(const Layouts&... layouts) const {
         return MakeTensor(Data(), Layout().Tile(layouts...));
     }
+
+    __aicore__ inline constexpr void SetL2CacheHint(CacheMode mode) {
+        Engine().SetL2CacheHint(mode);
+    }
 };
 
 template <typename T>
@@ -177,16 +181,16 @@ struct HasDereference<T, void_t<decltype(*Std::declval<T&>())>> : Std::true_type
 template <typename T>
 struct MakeLocalTensor {
 template <typename Arg0, typename... Args>
-__aicore__ inline constexpr auto operator()(const Arg0& arg0, const Args&... args) const {
-    if constexpr (HasDereference<Arg0>::value) {
-    using Engine = ViewEngine<Arg0>;
-    if constexpr (sizeof...(Args) == 1 && (is_layout<Args>::value && ...)) {
-        return LocalTensor<TensorAttribute<Engine, Args...>>{Engine{arg0}, args...};
-    } else {
-        return LocalTensor<TensorAttribute<Engine, decltype(MakeLayout(args...))>>{Engine{arg0}, MakeLayout(args...)};
+    __aicore__ inline constexpr auto operator()(const Arg0& arg0, const Args&... args) const {
+        if constexpr (HasDereference<Arg0>::value) {
+            using Engine = ViewEngine<Arg0>;
+            if constexpr (sizeof...(Args) == 1 && (is_layout<Args>::value && ...)) {
+                return LocalTensor<TensorAttribute<Engine, Args...>>{Engine{arg0}, args...};
+            } else {
+                return LocalTensor<TensorAttribute<Engine, decltype(MakeLayout(args...))>>{Engine{arg0}, MakeLayout(args...)};
+            }
+        }
     }
-    }
-}
 };
 
 template <typename Iterator, typename... Args>
---
+++ tests/api/tensor_api/npu_arch_3510/aic/test_tensor_api_tensor.cpp
@@ -0,0 +1,39 @@
+/**
+* Copyright (c) 2026 Huawei Technologies Co., Ltd.
+* This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+* CANN Open Software License Agreement Version 2.0 (the "License").
+* Please refer to the License for details. You may not use this file except in compliance with the License.
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+* See LICENSE in the root of the software repository for the full text of the License.
+*/
+#include <gtest/gtest.h>
+#include "tensor_api/stub/cce_stub.h"
+#include "include/experimental/tensor_api/tensor.h"
+
+class Tensor_Api_Tensor_3510 : public testing::Test {
+protected:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    virtual void SetUp() {}
+    void TearDown() {}
+};
+
+TEST_F(Tensor_Api_Tensor_3510, SetL2CacheHint)
+{
+    using namespace AscendC::Te;
+
+    constexpr uint32_t TILE_LENGTH = 8;
+    __gm__ float data[TILE_LENGTH] = {0, 1, 2, 3, 4, 5, 6, 7};
+    auto ptr = MakeGMmemPtr(data);
+    auto tensor = MakeTensor(ptr, MakeShape(AscendC::Std::Int<2>{}, AscendC::Std::Int<2>{}, AscendC::Std::Int<2>{}),
+                             MakeStride(AscendC::Std::Int<4>{}, AscendC::Std::Int<2>{}, AscendC::Std::Int<1>{}));
+
+    EXPECT_EQ(tensor.Data().Get(), data);
+
+    tensor.SetL2CacheHint(CacheMode::CACHE_MODE_DISABLE);
+    EXPECT_NE(tensor.Data().Get(), data);
+
+    tensor.SetL2CacheHint(CacheMode::CACHE_MODE_NORMAL);
+    EXPECT_EQ(tensor.Data().Get(), data);
+}
