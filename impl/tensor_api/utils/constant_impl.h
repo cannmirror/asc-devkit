ff9dbcf6ad4a5ecf0248a1e99eb614116a2f217a
yue_mo_du_qian <gaoming39@huawei.com>
2026-04-14 15:40:05 +0800

    tensor_api_experimental_locationAttr
    Co-authored-by: gaoming<gaoming39@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1596 merge tensor_api_experimental_locationAttr into tensor_api_experimental
    
    tensor_api_experimental_locationAttr
    
    Created-by: yue_mo_du_qian
    Commit-by: gaoming
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #xxx-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [x] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!1596

--- impl/tensor_api/utils/constant_impl.h
+++ impl/tensor_api/utils/constant_impl.h
@@ -29,10 +29,6 @@ namespace Te {
 constexpr size_t TWO_DIM_DATA = 2;
 constexpr size_t FOUR_DIM_DATA = 4;
 constexpr size_t FRACTAL_FIXED = 16;
-constexpr size_t DISABLE_COORD = 0;
-constexpr size_t ENABLE_COORD = 1;
-constexpr size_t SHIFT_LEFT_16 = 0x00010000;
-constexpr size_t L2_CACHE_OFFSET = 60;
 constexpr size_t MX_SCALE_K0 = 2;
 constexpr uint32_t BLOCK_CUBE = 16;
 
@@ -54,8 +50,6 @@ struct GetArchVersion {
 constexpr uint32_t CURRENT_ARCH_VERSION = GetArchVersion{}();
 
 enum class LayoutFormat : uint8_t { NZ, ZN, ZZ, DN, ND, NN};
-
-enum class TupleFormat : uint8_t { Shape, Stride, Coord};
 
 namespace Location {
     struct EmptyTrait {};
@@ -79,7 +73,19 @@ __aicore__ inline constexpr auto GetHardPos()
 }
 
 template <typename T>
-using locationAttr = Std::tuple<__gm__ T*, __cbuf__ T*, __ca__ T*, __cb__ T*, __cc__ T*, __ubuf__ T*, __fbuf__ T*, __ssbuf__ T*, __biasbuf__ T*>;
+struct locationAttr {
+    using gmAttr =          __gm__      T*;
+    using cbufAttr =        __cbuf__    T*;
+    using caAttr =          __ca__      T*;
+    using cbAttr =          __cb__      T*;
+    using ccAttr =          __cc__      T*;
+    using ubufAttr =        __ubuf__    T*;
+    using fbufAttr =        __fbuf__    T*;
+    using ssbufAttr =       __ssbuf__   T*;
+    using biasbufAttr =     __biasbuf__ T*;
+
+    using type = Std::tuple<gmAttr, cbufAttr, caAttr, cbAttr, ccAttr, ubufAttr, fbufAttr, ssbufAttr, biasbufAttr>;    
+};
 
 template <typename A, typename ProcessedTuple>
 struct AllElementsSameAsA;
@@ -97,7 +103,7 @@ struct CheckAllSame;
 
 template <typename A, typename B, typename... RestB>
 struct CheckAllSame<A, B, RestB...> {
-    static constexpr bool value =  Std::is_same_v<A, B> || AllElementsSameAsA<A, locationAttr<B>>::value || CheckAllSame<A, RestB...>::value;
+    static constexpr bool value =  Std::is_same_v<A, B> || AllElementsSameAsA<A, typename locationAttr<B>::type>::value || CheckAllSame<A, RestB...>::value;
 };
 
 template <typename A>
