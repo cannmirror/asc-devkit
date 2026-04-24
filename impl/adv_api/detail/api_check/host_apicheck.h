81f5284904b6809ac47b533b329757c299cd3f22
Zql99 <zhangqianlong8@huawei.com>
2026-04-24 17:31:02 +0800

    fix:host侧告警提示
    Co-authored-by: Zql99<zhangqianlong8@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1743 merge master into master
    
    fix:host侧告警提示
    
    Created-by: Zql99
    Commit-by: Zql99
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    host侧存在使用内部头文件告警，需要将其去除
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #xxx-->
    [#520](https://gitcode.com/cann/asc-devkit/issues/520) 
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    NA
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!1743

--- impl/adv_api/detail/api_check/host_apicheck.h
+++ impl/adv_api/detail/api_check/host_apicheck.h
@@ -12,13 +12,6 @@
  * \file host_apicheck.h
  * \brief
  */
-#if !defined(__ASCENDC_INCLUDE_INTERNAL_HEADERS__)
-#pragma message( \
-    "impl/adv_api/detail/api_check/host_apicheck.h is an internal header file and must not be used directly. Functions or variables defined in this file may be removed in the future. Please use \"#include \"adv_api/tiling_api.h\"\" and use public functions or variables defined in interface headers files.")
-#define __ASCENDC_INCLUDE_INTERNAL_HEADERS__
-#define __UNDEF_ASCENDC_INCLUDE_INTERNAL_HEADERS_HOST_APICHECK_H__
-#endif
-
 #ifndef IMPL_HOST_APICHECK_H
 #define IMPL_HOST_APICHECK_H
 
@@ -78,8 +71,3 @@ inline void IsReuseSourceVerifyingParameters(const bool isReuseSource)
 } // namespace HighLevelApiCheck
 } // namespace AscendC
 #endif // IMPL_HOST_APICHECK_H
-
-#if defined(__UNDEF_ASCENDC_INCLUDE_INTERNAL_HEADERS_HOST_APICHECK_H__)
-#undef __ASCENDC_INCLUDE_INTERNAL_HEADERS__
-#undef __UNDEF_ASCENDC_INCLUDE_INTERNAL_HEADERS_HOST_APICHECK_H__
-#endif
